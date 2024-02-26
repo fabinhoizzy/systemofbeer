d847cbab641c35f6ca0a9cb03e799179a6550071
Fabio Silva <fabinhoevil16@hotmail.com>
2024-02-26 19:54:26 -0300

    fix: fixed wrong route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 declare(strict_types=1);
 
 use App\Http\Controllers\Frontend\QrCodeController;
+use Filament\Facades\Filament;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,7 +17,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return to_route('filament.admin.auth.register');
+    $panelID = Filament::getPanel()->getId();
+    return to_route("filament.{$panelID}.pages.dashboard");
 })->name('frontend.home');
 
 Route::get('/qr-code', QrCodeController::class)->name('frontend.qr-code');
